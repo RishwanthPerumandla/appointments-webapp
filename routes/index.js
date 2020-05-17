2782184ef982adce39c9a715d9d8e8f5b3395052
skillbattle <rishwanth100@gmail.com>
2020-05-17 12:40:22 +0530

    Mail Automated

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,23 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
 const FormData = require("../models/FormData");
+const nodemailer = require('nodemailer');
+
 // Welcome Page
 // router.get('/', forwardAuthenticated, async (req, res) => {res.render('welcome')
 
 // });
+
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'grietconnect@gmail.com',
+    pass: 'grietconnect@123'
+  }
+});
+
 
 router.get("/", (req, res) => {
   res.render("home");
@@ -17,10 +30,12 @@ router.get("/dashboard", ensureAuthenticated, async (req, res) => {
     person: userrole,
     responses: undefined,
   });
+  
   const appointmentsList = await FormData.find({
     person: userrole,
     responses: true,
   });
+
   const declinedList = await FormData.find({
     person: userrole,
     responses: false,
@@ -36,18 +51,52 @@ router.get("/dashboard", ensureAuthenticated, async (req, res) => {
 
 router.get("/edit/:id", ensureAuthenticated, async (req, res) => {
   await FormData.findByIdAndUpdate(req.params.id, { responses: true });
-
+  const person = await FormData.findById(req.params.id);
+  console.log(person.email);
+  var mailOptions = {
+    from: 'grietconnect@gmail.com',
+    to: person.email,
+    subject: 'Response For Your Appointment',
+    text: `Your Appointment on ${person.date} with ${person.person} is Approved`
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
   res.redirect("/dashboard");
 });
 
 router.get("/delete/:id", ensureAuthenticated, async (req, res) => {
   await FormData.findByIdAndUpdate(req.params.id, { responses: false });
-
+  const person = await FormData.findById(req.params.id);
+  console.log(person.email);
+  var mailOptions = {
+    from: 'grietconnect@gmail.com',
+    to: person.email,
+    subject: 'Response For Your Appointment',
+    text: `Your Appointment on ${person.date} with ${person.person} is Declined`
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
   res.redirect("/dashboard");
 });
 
-router.get("/redirect", (req, res) => {
+router.get("/redirect",(req, res) => {
   res.render("redirect");
 });
 
 module.exports = router;
+
+
+
+
